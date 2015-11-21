7cb12a82af32907f6cbf12ed3a4377eb5fde8cb4
Ricardo Tealdi <ricardo.tealdi@gmail.com>
2015-11-21 10:51:34 -0200

    Add `logger` to `redis`

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,6 @@
 Rails.configuration.redis_pool = ConnectionPool.new(size: 10, timeout: 5) do
-  Redis.new(url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/0'))
+  Redis.new(
+    url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/0'),
+    logger: Rails.logger
+  )
 end
